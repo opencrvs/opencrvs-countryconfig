f076cba914032c71dbd0595813aa7fa3b26a6361
Riku Rouvila <riku.rouvila@gmail.com>
2020-02-05 16:46:48 +0200

    add missing files

---
+++ start-prod.sh
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# OpenCRVS is also distributed under the terms of the Civil Registration
+# & Healthcare Disclaimer located at http://opencrvs.org/license.
+#
+# Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+# graphic logo are (registered/a) trademark(s) of Plan International.
+
+set -e
+
+sed -i "s/{{hostname}}/$HOSTNAME/g" src/bgd/config/client-config.prod.js
+sed -i "s/{{hostname}}/$HOSTNAME/g" src/bgd/config/login-config.prod.js
+
+yarn start:prod
