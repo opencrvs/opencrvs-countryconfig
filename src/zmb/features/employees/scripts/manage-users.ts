8a4d2a6001045d2cdd5eeb43eecfbea1525a42a1
Sadman Anik <sadman.anik@dsinnovators.com>
2021-11-25 18:41:33 +0600

    Added new National Sysytem User

--- src/zmb/features/employees/scripts/manage-users.ts
+++ src/zmb/features/employees/scripts/manage-users.ts
@@ -41,6 +41,8 @@ export function getScope(role: string, environment: string): string[] {
       )
     case 'LOCAL_SYSTEM_ADMIN':
       return setDemoUser(['sysadmin'], environment)
+    case 'NATIONAL_SYSTEM_ADMIN':
+      return setDemoUser(['sysadmin'], environment)
     case 'CHATBOT_API_USER':
       return setDemoUser(['declare', 'chatbot-api'], environment)
     default:
