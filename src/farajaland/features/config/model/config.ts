2b4c76304d6c5253b6502892163a3ec21fd0778f
Atiq Zaman <atiq.zaman@dsinnovators.com>
2022-03-30 17:48:40 +0600

    App config feature branch (#210)
    * Added new messages for application config
    
    * Added application name change messages
    
    * Added APPLICATION_NAME on application config model and backup
    
    * Added application name change notification messages
    
    * Added some new application config messages
    
    * Added currency on config
    
    * NID_NUMBER_PATTERN refactored, message added for NID modal
    
    * Added messaged for currency and removed unused messages
    
    * Added currency on window config
    
    * Created backup of application config
    
    * Added contents for govt logo change
    
    * Removed CERTIFICATE_PRINT_CHARGE_FREE_PERIOD & CERTIFICATE_PRINT_CHARGE_UP_LIMIT
    
    * Added LATE_BIRTH_REGISTRATION_TARGET on application config
    
    * Changed Birth and Death config structure
    
    * Updated application-config backup
    
    * backup for PHONE_NUMBER_PATTERN changed
    
    * Removed CERTIFICATE_PRINT_LOWEST_CHARGE & CERTIFICATE_PRINT_HIGHEST_CHARGE
    
    * message added for phone number pattern change modal
    
    * Modified certificate price messages
    
    * Fixed Birth and Death Config messages
    
    * Added new messages for birth target days modal
    
    * Added new messages for death target days modal
    
    * Added new messages for update birth fee modal
    
    * Added new messages for death fee modal
    
    * Merged with base app-config-feature-branch
    
    * Removed currency from config file
    
    Co-authored-by: Sadman Anik <sadman.anik@dsinnovators.com>
    Co-authored-by: emon-swe-sust <mahfuzur03@student.sust.edu>
    Co-authored-by: soumita-dsi <soumitaghosh.dsi@gmail.com>

--- src/farajaland/features/config/model/config.ts
+++ src/farajaland/features/config/model/config.ts
@@ -9,8 +9,25 @@
  * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
  * graphic logo are (registered/a) trademark(s) of Plan International.
  */
+import { string } from 'joi'
 import { model, Schema, Document } from 'mongoose'
 
+interface IBirth {
+  REGISTRATION_TARGET: number
+  LATE_REGISTRATION_TARGET: number
+  FEE: {
+    ON_TIME: number
+    LATE: number
+    DELAYED: number
+  }
+}
+interface IDeath {
+  REGISTRATION_TARGET: number
+  FEE: {
+    ON_TIME: number
+    DELAYED: number
+  }
+}
 interface IPhoneNumberPattern {
   pattern: RegExp
   example: string
@@ -21,25 +38,22 @@ interface IPhoneNumberPattern {
     endBefore: number
   }
 }
-
-interface INIDNumberPattern {
-  pattern: RegExp
-  example: string
-  num: string
+interface ICurrency {
+  isoCode: string
+  languagesAndCountry: string[]
 }
-
 export interface IApplicationConfigurationModel extends Document {
+  APPLICATION_NAME: string
   BACKGROUND_SYNC_BROADCAST_CHANNEL: string
+  BIRTH : IBirth
   COUNTRY: string
   COUNTRY_LOGO_FILE: string
   COUNTRY_LOGO_RENDER_WIDTH: number
   COUNTRY_LOGO_RENDER_HEIGHT: number
+  CURRENCY: ICurrency
+  DEATH: IDeath
   DESKTOP_TIME_OUT_MILLISECONDS: number
   LANGUAGES: string
-  CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: number
-  CERTIFICATE_PRINT_CHARGE_UP_LIMIT: number
-  CERTIFICATE_PRINT_LOWEST_CHARGE: number
-  CERTIFICATE_PRINT_HIGHEST_CHARGE: number
   UI_POLLING_INTERVAL: number
   FIELD_AGENT_AUDIT_LOCATIONS: string
   DECLARATION_AUDIT_LOCATIONS: string
@@ -48,60 +62,48 @@ export interface IApplicationConfigurationModel extends Document {
   EXTERNAL_VALIDATION_WORKQUEUE: boolean
   SENTRY: string
   LOGROCKET: string
-  PHONE_NUMBER_PATTERN: IPhoneNumberPattern
-  BIRTH_REGISTRATION_TARGET: number
-  DEATH_REGISTRATION_TARGET: number
-  NID_NUMBER_PATTERN: INIDNumberPattern
+  PHONE_NUMBER_PATTERN: string
+  NID_NUMBER_PATTERN: string
 }
 
-const nidPatternSchema = new Schema<INIDNumberPattern>({
-  pattern: { type: String },
-  example: String,
-  num: String
-})
-
-const phoneNumberSchema = new Schema<IPhoneNumberPattern>({
-  pattern: { type: String },
-  example: String,
-  start: String,
-  num: String,
-  mask: {
-    startForm: Number,
-    endBefore: Number
+const birthSchema = new Schema<IBirth>({
+  REGISTRATION_TARGET: { type: Number, default: 45 },
+  LATE_REGISTRATION_TARGET: { type: Number, default: 365 },
+  FEE: {
+    ON_TIME: Number,
+    LATE: Number,
+    DELAYED: Number
   }
 })
 
+const deathSchema = new Schema<IDeath>({
+  REGISTRATION_TARGET: { type: Number, default: 45 },
+  FEE: {
+    ON_TIME: Number,
+    DELAYED: Number
+  }
+})
+
+
+const currencySchema = new Schema<IPhoneNumberPattern>({
+  isoCode: { type: String },
+  languagesAndCountry: [String]
+})
+
 const systemSchema = new Schema({
+  APPLICATION_NAME: { type: String, require: false, default: 'OpenCRVS' },
   BACKGROUND_SYNC_BROADCAST_CHANNEL: { type: String, required: false },
+  BIRTH: { type: birthSchema, required: false },
   COUNTRY: { type: String, required: false },
   COUNTRY_LOGO_FILE: { type: String, required: false },
   COUNTRY_LOGO_RENDER_WIDTH: { type: Number, required: false, default: 104 },
   COUNTRY_LOGO_RENDER_HEIGHT: { type: Number, required: false, default: 104 },
+  CURRENCY: { type: currencySchema, require: false },
+  DEATH: { type: deathSchema, required: false },
   DESKTOP_TIME_OUT_MILLISECONDS: {
     type: Number,
     required: false,
     default: 900000
-  },
-  LANGUAGES: { type: String, required: false, default: 'en' },
-  CERTIFICATE_PRINT_CHARGE_FREE_PERIOD: {
-    type: Number,
-    required: false,
-    default: 36500
-  },
-  CERTIFICATE_PRINT_CHARGE_UP_LIMIT: {
-    type: Number,
-    required: false,
-    default: 36500
-  },
-  CERTIFICATE_PRINT_LOWEST_CHARGE: {
-    type: Number,
-    required: false,
-    default: 0
-  },
-  CERTIFICATE_PRINT_HIGHEST_CHARGE: {
-    type: Number,
-    required: false,
-    default: 0
   },
   UI_POLLING_INTERVAL: { type: Number, required: false, default: 5000 },
   FIELD_AGENT_AUDIT_LOCATIONS: {
@@ -125,7 +127,6 @@ const systemSchema = new Schema({
     required: false,
     default: false
   },
-  PHONE_NUMBER_PATTERN: { type: phoneNumberSchema, required: false },
   BIRTH_REGISTRATION_TARGET: {
     type: Number,
     required: false,
@@ -136,7 +137,8 @@ const systemSchema = new Schema({
     required: false,
     default: 45
   },
-  NID_NUMBER_PATTERN: { type: nidPatternSchema, required: false },
+  PHONE_NUMBER_PATTERN: { type: String, required: false },
+  NID_NUMBER_PATTERN: { type: String, required: false },
   SENTRY: { type: String, required: false },
   LOGROCKET: { type: String, required: false }
 })
