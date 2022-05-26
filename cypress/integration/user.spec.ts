6c8e78eb048df7130c194eec49c1266c9a3e62ca
euanmillar <euanmillar77@gmail.com>
2022-05-26 15:56:21 +0100

    Amended cypress test

--- cypress/integration/user.spec.ts
+++ cypress/integration/user.spec.ts
@@ -26,8 +26,8 @@ context('User Integration Test', () => {
     cy.get('#familyNameEng').type('Ahmed')
     cy.get('#phoneNumber').type('0711919045')
     //cy.get('#nid').type('199475632')
-    cy.selectOption('#role', 'Registration Clerk', 'Registration Clerk')
-    cy.selectOption('#type', 'Data entry clerk', 'Data entry clerk')
+    cy.selectOption('#role', 'Field Agent', 'Field Agent')
+    cy.selectOption('#type', 'Healthcare Worker', 'Healthcare Worker')
     cy.get('#device').type('Xiamoi MI 8')
     cy.get('#confirm_form').click()
     // PREVIEW
