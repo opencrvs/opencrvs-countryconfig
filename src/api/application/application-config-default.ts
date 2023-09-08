144d6471e9afd495c3a2597d6682cdeb71e8b88d
euanmillar <euanmillar77@gmail.com>
2023-09-08 17:31:22 +0100

    Remove public keys unused imports and reorder app setting

--- src/api/application/application-config-default.ts
+++ src/api/application/application-config-default.ts
@@ -2,9 +2,6 @@ import { countryLogo } from '@countryconfig/api/application/country-logo'
 
 export const defaultApplicationConfig = {
   APPLICATION_NAME: 'Farajaland CRS',
-  FIELD_AGENT_AUDIT_LOCATIONS: 'DISTRICT',
-  DECLARATION_AUDIT_LOCATIONS: 'DISTRICT',
-  EXTERNAL_VALIDATION_WORKQUEUE: false,
   BIRTH: {
     REGISTRATION_TARGET: 30,
     LATE_REGISTRATION_TARGET: 365,
@@ -15,11 +12,6 @@ export const defaultApplicationConfig = {
     },
     PRINT_IN_ADVANCE: true
   },
-  COUNTRY_LOGO: countryLogo,
-  CURRENCY: {
-    languagesAndCountry: ['en-US'],
-    isoCode: 'USD'
-  },
   DEATH: {
     REGISTRATION_TARGET: 45,
     FEE: {
@@ -28,11 +20,7 @@ export const defaultApplicationConfig = {
     },
     PRINT_IN_ADVANCE: true
   },
-  PHONE_NUMBER_PATTERN: '^0(7|9)[0-9]{8}$',
-  NID_NUMBER_PATTERN: '^[0-9]{10}$',
-  LOGIN_BACKGROUND: {
-    backgroundColor: '36304E'
-  },
+  MARRIAGE_REGISTRATION: false,
   MARRIAGE: {
     REGISTRATION_TARGET: 45,
     FEE: {
@@ -41,12 +29,24 @@ export const defaultApplicationConfig = {
     },
     PRINT_IN_ADVANCE: true
   },
-  MARRIAGE_REGISTRATION: false,
   DATE_OF_BIRTH_UNKNOWN: false,
+  CURRENCY: {
+    languagesAndCountry: ['en-US'],
+    isoCode: 'USD'
+  },
+  PHONE_NUMBER_PATTERN: '^0(7|9)[0-9]{8}$',
+  NID_NUMBER_PATTERN: '^[0-9]{10}$',
+  COUNTRY_LOGO: countryLogo,
+  LOGIN_BACKGROUND: {
+    backgroundColor: '36304E'
+  },
   INFORMANT_SIGNATURE: false,
   INFORMANT_SIGNATURE_REQUIRED: false,
+  FIELD_AGENT_AUDIT_LOCATIONS: 'DISTRICT',
+  DECLARATION_AUDIT_LOCATIONS: 'DISTRICT',
   USER_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
-  INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'email' // or 'sms', or '' ... You can use 'sms' for WhatsApp
+  INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
+  EXTERNAL_VALIDATION_WORKQUEUE: false
 }
 
 export const COUNTRY_WIDE_CRUDE_DEATH_RATE = 10
