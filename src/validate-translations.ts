952459495e5071dadb533b94324b1e1b5e78c620
Md. Ashikul Alam <nileeeem36@gmail.com>
2023-09-11 18:27:07 +0600

    validate translation script add

---
+++ src/validate-translations.ts
@@ -0,0 +1,69 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * OpenCRVS is also distributed under the terms of the Civil Registration
+ * & Healthcare Disclaimer located at http://opencrvs.org/license.
+ *
+ * Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
+ * graphic logo are (registered/a) trademark(s) of Plan International.
+ */
+import chalk from 'chalk'
+import * as fs from 'fs'
+import { join } from 'path'
+import { ILanguage } from './api/content/service'
+
+async function validateMessages() {
+  let allMessageData!: {
+    data: Array<{
+      lang: string
+      displayName: string
+      messagess: Record<string, string>
+    }>
+  }
+
+  try {
+    allMessageData = JSON.parse(
+      fs
+        .readFileSync(join(__dirname, `./api/content/client/client.json`))
+        .toString()
+    )
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  const availableTranslations = allMessageData.data.map(
+    (d: Partial<ILanguage>) => ({
+      lang: d.displayName,
+      messages: d.messages
+    })
+  )
+
+  const allTranslationKeysSet = new Set()
+
+  availableTranslations.forEach((item) => {
+    const messageKeys = Object.keys(item.messages!)
+    messageKeys.forEach((key) => {
+      allTranslationKeysSet.add(key)
+    })
+  })
+
+  let missingKey = false
+  allTranslationKeysSet.forEach((item: string) => {
+    availableTranslations.map(({ lang, messages }) => {
+      if (!Object.keys(messages!).includes(item)) {
+        missingKey = true
+        console.log(`${chalk.white(`${lang}`)} ${chalk.red(
+          `translations missing for this key: `
+        )} ${chalk.green(`${item}`)}  
+          `)
+      }
+    })
+  })
+
+  if (!missingKey) console.log('No translations are missing')
+}
+
+validateMessages()
