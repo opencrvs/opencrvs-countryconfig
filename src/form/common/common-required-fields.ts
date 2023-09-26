38f7b08b90c56dcbfc60df1b05d59922af7386e3
Pyry <pyry.rouvila@gmail.com>
2023-09-26 13:39:44 +0300

    make national id required

--- src/form/common/common-required-fields.ts
+++ src/form/common/common-required-fields.ts
@@ -149,7 +149,7 @@ export const getNationalID = (
     name: fieldName,
     type: 'TEXT',
     label: formMessageDescriptors.iDTypeNationalID,
-    required: false,
+    required: true,
     initialValue: '',
     validator,
     conditionals,
