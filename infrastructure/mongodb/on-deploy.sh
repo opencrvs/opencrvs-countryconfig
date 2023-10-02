fb5736dfae4ec714ce0c6e84797adb663df17bec
euanmillar <euanmillar77@gmail.com>
2023-10-02 16:10:44 +0100

    Update licence

--- infrastructure/mongodb/on-deploy.sh
+++ infrastructure/mongodb/on-deploy.sh
@@ -6,8 +6,7 @@
 # OpenCRVS is also distributed under the terms of the Civil Registration
 # & Healthcare Disclaimer located at http://opencrvs.org/license.
 #
-# Copyright (C) The OpenCRVS Authors. OpenCRVS and the OpenCRVS
-# graphic logo are (registered/a) trademark(s) of Plan International.
+# Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
 
 # This file is run on each deployment with the sole purpose of updating
 # passwords of MongoDB users to passwords given to this service as environment varibles
