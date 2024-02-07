71b7963647f503db2c4d3434d899b0ab030b18de
Riku Rouvila <riku.rouvila@gmail.com>
2024-02-07 20:01:38 +0900

    Fix syslog errors coming from missing metricbeat capabilities (#889)
    * add ptrace capability for metricbeat so syslog isn't filled with errors
    
    * update known hosts entry for dev
    
    * fix debug syntax
    
    * add IP address entries also to known-hosts file
    
    * cleanup

--- infrastructure/environments/update-known-hosts.sh
+++ infrastructure/environments/update-known-hosts.sh
@@ -9,6 +9,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 DOMAIN=$1
+IP=$(dig +short $DOMAIN)
 KNOWN_HOSTS_FILE="infrastructure/known-hosts"
 
 # Ensure the known-hosts file exist
@@ -16,6 +17,7 @@ touch "$KNOWN_HOSTS_FILE"
 
 # Remove existing entry for the domain from the known-hosts file
 ssh-keygen -R "$DOMAIN" -f "$KNOWN_HOSTS_FILE" || true
+ssh-keygen -R "$IP" -f "$KNOWN_HOSTS_FILE" || true
 
 # Initialize keyscan result variable
 KEYSCAN_RESULT=""
@@ -23,7 +25,7 @@ KEYSCAN_RESULT=""
 # Attempt to fetch the new SSH public key for the domain
 while [ -z "$KEYSCAN_RESULT" ]; do
   # Use `|| true` to prevent script exit if ssh-keyscan fails
-  KEYSCAN_RESULT=$(ssh-keyscan "$DOMAIN" 2>/dev/null) || true
+  KEYSCAN_RESULT=$(ssh-keyscan "$DOMAIN" "$IP" 2>/dev/null) || true
 
   # Check if ssh-keyscan was successful
   if [ -z "$KEYSCAN_RESULT" ]; then
