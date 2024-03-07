23d29b9db782ff208ae4b2c6d5f0dfc637d052b3
Riku Rouvila <riku.rouvila@gmail.com>
2024-03-07 14:42:31 +0200

    add unit test CI pipeline back

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,15 +11,8 @@
 import { readFileSync } from 'fs'
 import * as jwt from 'jsonwebtoken'
 import { createServer } from '@countryconfig/index'
-import * as locationsService from '@countryconfig/features/administrative/service/service'
 
 describe('Route authorization', () => {
-  beforeAll(() => {
-    jest
-      .spyOn(locationsService, 'getStatistics')
-      .mockReturnValue(Promise.resolve([]))
-  })
-
   it('tests the health check', async () => {
     const server = await createServer()
     const res = await server.server.inject({
