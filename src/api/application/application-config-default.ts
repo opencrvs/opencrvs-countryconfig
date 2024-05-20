186daf286afff54ea20c6b182f5ca8b114c8e3f0
Md. Ashikul Alam <32668488+Nil20@users.noreply.github.com>
2024-05-20 15:41:28 +0600

    chore!: move configuration options (#1005)

--- src/api/application/application-config-default.ts
+++ src/api/application/application-config-default.ts
@@ -46,10 +46,10 @@ export const defaultApplicationConfig = {
     MARRIAGE_REGISTRATION: true,
     EXTERNAL_VALIDATION_WORKQUEUE: false,
     INFORMANT_SIGNATURE: true,
-    PRINT_DECLARATION: true
+    PRINT_DECLARATION: true,
+    DATE_OF_BIRTH_UNKNOWN: true,
+    INFORMANT_SIGNATURE_REQUIRED: false
   },
-  DATE_OF_BIRTH_UNKNOWN: true,
-  INFORMANT_SIGNATURE_REQUIRED: false,
   USER_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
   INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
   SIGNATURE_REQUIRED_FOR_ROLES: ['LOCAL_REGISTRAR', 'NATIONAL_REGISTRAR']
