2c59402fdc77b9653638e925c08736447a1eec64
Riku Rouvila <riku.rouvila@gmail.com>
2024-05-31 10:42:40 +0300

    add error to deployment script if user hasnt set up known hosts properly

--- infrastructure/deployment/deploy.sh
+++ infrastructure/deployment/deploy.sh
@@ -342,6 +342,17 @@ echo "Deploying COUNTRY_CONFIG_VERSION $COUNTRY_CONFIG_VERSION to $SSH_HOST..."
 echo
 echo "Syncing configuration files to the target server"
 
+if ! ssh-keygen -l -F $SSH_HOST -f $INFRASTRUCTURE_DIRECTORY/known-hosts; then
+  echo "Host key for $SSH_HOST not found in $INFRASTRUCTURE_DIRECTORY/known-hosts. Please add the host key to the known-hosts file."
+  echo "You cando this by running the following command:"
+  echo "sh ./infrastructure/environments/update-known-hosts.sh <YOUR DOMAIN>"
+  echo ""
+  echo "or"
+  echo ""
+  echo "sh ./infrastructure/environments/update-known-hosts.sh <YOUR SERVER IP>"
+  exit 1
+fi
+
 configured_rsync -rlD $PROJECT_ROOT/infrastructure $SSH_USER@$SSH_HOST:/opt/opencrvs/ --delete --no-perms --omit-dir-times --verbose
 configured_rsync -rlD /tmp/docker-compose.yml /tmp/docker-compose.deps.yml $SSH_USER@$SSH_HOST:/opt/opencrvs/infrastructure --no-perms --omit-dir-times  --verbose
 
