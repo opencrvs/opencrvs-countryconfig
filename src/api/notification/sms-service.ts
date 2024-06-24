8fc9ddee037c42d9478ad6f2d9b385d613cb4cd4
naftis <pyry.rouvila@gmail.com>
2024-06-24 21:38:59 +0300

    fix: sms templates not being to able to parse json

--- src/api/notification/sms-service.ts
+++ src/api/notification/sms-service.ts
@@ -15,10 +15,9 @@ import {
 } from './constant'
 import { logger } from '@countryconfig/logger'
 import fetch from 'node-fetch'
-import { readFileSync } from 'fs'
 import * as Handlebars from 'handlebars'
-import { join } from 'path'
 import { internal } from '@hapi/boom'
+import { getLanguages } from '../content/service'
 
 export const informantTemplates = {
   birthInProgressNotification: 'birthInProgressNotification',
@@ -42,12 +41,6 @@ const otherTemplates = {
 export type SMSTemplateType =
   | keyof typeof otherTemplates
   | keyof typeof informantTemplates
-
-interface ISMSNotificationTemplate {
-  lang: string
-  displayName: string
-  messages: Record<SMSTemplateType, string>
-}
 
 export async function sendSMS(
   type: SMSTemplateType,
@@ -98,21 +91,19 @@ export async function sendSMS(
   }
 }
 
-function compileMessages(
+const compileMessages = async (
   templateName: SMSTemplateType,
   variables: Record<string, string>,
   locale: string
-): string {
-  const smsNotificationTemplate = JSON.parse(
-    readFileSync(
-      join(__dirname, '../languages/content/notification/notification.json')
-    ).toString()
-  ).data as ISMSNotificationTemplate[]
+) => {
+  const languages = await getLanguages('notification')
+  const language = languages.find(({ lang }) => lang === locale)
 
-  const language = smsNotificationTemplate.filter((obj) => {
-    return obj.lang === locale
-  })[0]
+  if (!language) {
+    throw new Error(
+      `Locale "${locale}" not found while compiling notification messages.`
+    )
+  }
 
-  const template = Handlebars.compile(language.messages[templateName])
-  return template(variables)
+  return Handlebars.compile(language.messages[templateName])(variables)
 }
