d5b44ca95a120063d1a9126f792a0c69cd866d1f
NantenainaRochel2 <soniarimamys@gmail.com>
2024-09-05 16:18:28 +0300

    us sms as informant notification method

--- src/api/application/application-config-default.ts
+++ src/api/application/application-config-default.ts
@@ -56,7 +56,7 @@ export const defaultApplicationConfig = {
     INFORMANT_SIGNATURE_REQUIRED: false
   },
   USER_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
-  INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'email', // or 'sms', or '' ... You can use 'sms' for WhatsApp
+  INFORMANT_NOTIFICATION_DELIVERY_METHOD: 'sms', // or 'sms', or '' ... You can use 'sms' for WhatsApp
   SIGNATURE_REQUIRED_FOR_ROLES: ['LOCAL_REGISTRAR', 'NATIONAL_REGISTRAR']
 }
 
