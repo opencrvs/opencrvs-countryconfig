23d697edf50ea6b6c841fdc7a2914cb96591a89b
Riku Rouvila <riku.rouvila@gmail.com>
2024-09-09 16:18:29 +0300

    improvements to registry printing app demo

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+/*
+ * Check user has an authorization token. If not, redirect to login page.
+ */
 const token = new URLSearchParams(window.location.search).get('token')
 
 if (!token) {
@@ -7,7 +10,7 @@ if (!token) {
     encodeURIComponent(window.location.href)
 }
 
-async function downloadRecords() {
+async function downloadRecords({ startDate, endDate }) {
   const res = await fetch('/graphql', {
     headers: {
       authorization: 'Bearer ' + token,
@@ -17,13 +20,60 @@ async function downloadRecords() {
       operationName: 'searchEvents',
       variables: {
         advancedSearchParameters: {
-          registrationStatuses: ['REGISTERED', 'CERTIFIED', 'ISSUED']
+          registrationStatuses: ['REGISTERED', 'CERTIFIED', 'ISSUED'],
+          dateOfRegistrationStart: startDate,
+          dateOfRegistrationEnd: endDate
         },
         count: 10,
         skip: 0
       },
-      query:
-        'query searchEvents($advancedSearchParameters: AdvancedSearchParametersInput!, $sort: String, $count: Int, $skip: Int) {\n  searchEvents(\n    advancedSearchParameters: $advancedSearchParameters\n    sort: $sort\n    count: $count\n    skip: $skip\n  ) {\n    totalItems\n    results {\n      id\n      type\n      registration {\n        status\n        contactNumber\n        contactEmail\n        trackingId\n        registrationNumber\n        registeredLocationId\n        duplicates\n        assignment {\n          practitionerId\n          firstName\n          lastName\n          officeName\n          avatarURL\n          __typename\n        }\n        createdAt\n        modifiedAt\n        __typename\n      }\n      operationHistories {\n        operationType\n        operatedOn\n        __typename\n      }\n      ... on BirthEventSearchSet {\n        dateOfBirth\n        childName {\n          firstNames\n          middleName\n          familyName\n          use\n          __typename\n        }\n        __typename\n      }\n      ... on DeathEventSearchSet {\n        dateOfDeath\n        deceasedName {\n          firstNames\n          middleName\n          familyName\n          use\n          __typename\n        }\n        __typename\n      }\n      ... on MarriageEventSearchSet {\n        dateOfMarriage\n        brideName {\n          firstNames\n          middleName\n          familyName\n          use\n          __typename\n        }\n        groomName {\n          firstNames\n          middleName\n          familyName\n          use\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n'
+      query: `
+        query searchEvents($advancedSearchParameters: AdvancedSearchParametersInput!, $sort: String, $count: Int, $skip: Int) {
+          searchEvents(
+            advancedSearchParameters: $advancedSearchParameters
+            sort: $sort
+            count: $count
+            skip: $skip
+          ) {
+            totalItems
+            results {
+              id
+              type
+              registration {
+                status
+                contactNumber
+                contactEmail
+                trackingId
+                registrationNumber
+                registeredLocationId
+                duplicates
+                assignment {
+                  practitionerId
+                  firstName
+                  lastName
+                  officeName
+                  avatarURL
+                }
+                createdAt
+                modifiedAt
+              }
+              operationHistories {
+                operationType
+                operatedOn
+              }
+              ... on BirthEventSearchSet {
+                dateOfBirth
+                childName {
+                  firstNames
+                  middleName
+                  familyName
+                  use
+                }
+              }
+            }
+          }
+        }
+        `
     }),
     method: 'POST',
     mode: 'cors',
@@ -33,28 +83,101 @@ async function downloadRecords() {
   return res.json()
 }
 
-const $results = document.getElementById('results')
-downloadRecords()
-  .then((data) => renderTable(data.data.searchEvents.results))
-  .catch((err) => console.error(err))
+/*
+ * Initialise date inputs
+ */
+const $startDate = document.getElementById('start-date')
+const $endDate = document.getElementById('end-date')
+$startDate.value = new Date(new Date().getFullYear(), new Date().getMonth(), 1)
+  .toISOString()
+  .split('T')[0]
+$endDate.value = new Date(
+  new Date().getFullYear(),
+  new Date().getMonth() + 1,
+  0
+)
+  .toISOString()
+  .split('T')[0]
 
+$startDate.addEventListener('change', update)
+$endDate.addEventListener('change', update)
+
+/*
+ * Views
+ */
+
+function update() {
+  downloadRecords({ startDate: $startDate.value, endDate: $endDate.value })
+    .then((data) => renderTable(data.data.searchEvents.results))
+    .catch((err) => console.error(err))
+}
+
+const $results = document.getElementById('results')
+
+window.printAll = async function renderPrintout() {
+  const data = await downloadRecords({
+    startDate: $startDate.value,
+    endDate: $endDate.value
+  })
+  const results = data.data.searchEvents.results
+  const pages = results
+    .map((event) => {
+      /*
+       * Replace this with what ever you want to render for each page
+       */
+      const page = `
+      <div class="page">
+        <div class="p-8">
+          <h1>${event.childName[0].firstNames} ${event.childName[0].middleName} ${event.childName[0].familyName}</h1>
+          <h1>${event.dateOfBirth}</h1>
+        </div>
+      </div>
+    `
+
+      return page
+    })
+    .join('')
+
+  html2pdf()
+    .set({
+      pagebreak: { after: '.page' }
+    })
+    .from(pages)
+    .save()
+}
 function renderTable(results) {
   const rows = results
     .map((event) => {
-      let row = ''
-      if (event.__typename === 'BirthEventSearchSet') {
-        row = `
+      const row = `
       <tr>
-        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">${event.registration.trackingId}</td>
-        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${event.registration.registrationNumber}</td>
-        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${event.dateOfBirth}</td>
-        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${event.childName[0].firstNames} ${event.childName[0].middleName} ${event.childName[0].familyName}</td>
+        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">${
+          event.registration.trackingId
+        }</td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${
+          event.registration.registrationNumber
+        }</td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${
+          new Date(parseInt(event.registration.createdAt, 10))
+            .toISOString()
+            .split('T')[0]
+        }</td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${
+          event.dateOfBirth
+        }</td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">${
+          event.childName[0].firstNames
+        } ${event.childName[0].middleName} ${event.childName[0].familyName}</td>
       </tr>
     `
-      }
+
       return row
     })
     .join('')
 
   $results.innerHTML = rows
 }
+
+/*
+ * First render
+ */
+update()
