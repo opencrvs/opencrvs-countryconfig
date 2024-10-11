290144d0d7061164d6c27468d8663b6f3a9df6d3
HK <solo@digital.gov.mg>
2024-10-11 17:19:14 +0300

    fix undefined value in registry

--- public/main.js
+++ public/main.js
@@ -449,12 +449,12 @@ const renderTable = async () => {
     <td  class="px-4 py-2 border-b border-gray-300"><button class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded" onclick="openPrintModal('${
       item.id
     }', '${[
-      item.registration.assignment.firstName,
-      item.registration.assignment.lastName
+      item.registration?.assignment?.firstName || 'Nom par défaut',
+      item.registration?.assignment?.lastName || ''
     ]
       .join(' ')
       .trim()}', '${
-      item.registration.assignment.officeName
+      item.registration?.assignment?.officeName
     }')">Imprimer</button></td>
   `
     tableBody.appendChild(row)
