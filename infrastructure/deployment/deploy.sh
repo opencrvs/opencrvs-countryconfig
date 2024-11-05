97d7ba1b963f86293feee83afd8ddf4c5021ce92
jamil314 <jamil31415926@gmail.com>
2024-11-05 17:56:06 +0600

    fix: remove metabase.mv.db from docker container

--- infrastructure/deployment/deploy.sh
+++ infrastructure/deployment/deploy.sh
@@ -301,9 +301,9 @@ docker_stack_deploy() {
 
 reset_metabase() {
   echo "Reseting metabase"
-  configured_ssh 'docker service scale opencrvs_dashboards=0 && \
-    sudo rm /data/metabase/metabase.mv.db && \
-    docker service scale opencrvs_dashboards=1'
+  configured_ssh "docker exec \$(docker ps | grep opencrvs_dashboards | awk '{print \$1}' | head -n 1) /bin/sh -c \"rm /data/metabase/metabase.mv.db\" && \
+    docker service scale opencrvs_dashboards=0 && \
+    docker service scale opencrvs_dashboards=1"
 }
 
 validate_options
