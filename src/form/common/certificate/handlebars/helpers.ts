fa9e85acb99c3fe61a9529da7cb874ff68fbddb5
tareq89 <tareq.aziz@dsinnovators.com>
2024-11-06 16:45:46 +0600

    variables binding in sample svg certificates

--- src/form/common/certificate/handlebars/helpers.ts
+++ src/form/common/certificate/handlebars/helpers.ts
@@ -23,3 +23,30 @@ export function debug(): Handlebars.HelperDelegate {
     return value
   }
 }
+
+export function ordinalFormatDate(): Handlebars.HelperDelegate {
+  return function (dateString: string) {
+    const date = new Date(dateString.trim())
+    const getOrdinalSuffix = (day: number) => {
+      if (day > 3 && day < 21) return day + 'th'
+      switch (day % 10) {
+        case 1:
+          return day + 'st'
+        case 2:
+          return day + 'nd'
+        case 3:
+          return day + 'rd'
+        default:
+          return day + 'th'
+      }
+    }
+
+    // Format the date into ex: "11th November 2023"
+    const day = getOrdinalSuffix(date.getDate())
+    const month = date.toLocaleString('default', { month: 'long' })
+    const year = date.getFullYear()
+
+    const formattedDate = `${day} ${month} ${year}`
+    return formattedDate
+  }
+}
