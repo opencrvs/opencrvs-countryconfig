a779fbd30a96c5a2ed08929224ab113c3b865e60
tareq89 <tareqaziz.aust.cse@gmail.com>
2024-11-20 18:02:41 +0600

    added secondary font Libre Baskerville in birth certificate

--- src/api/certificates/handler.ts
+++ src/api/certificates/handler.ts
@@ -57,11 +57,11 @@ export async function certificateHandler(request: Request, h: ResponseToolkit) {
       },
       svgUrl: '/api/countryconfig/certificates/birth-certificate.svg',
       fonts: {
-        'Noto Sans': {
-          normal: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
-          bold: '/api/countryconfig/fonts/NotoSans-Bold.ttf',
-          italics: '/api/countryconfig/fonts/NotoSans-Regular.ttf',
-          bolditalics: '/api/countryconfig/fonts/NotoSans-Regular.ttf'
+        'Libre Baskerville': {
+          normal: '/api/countryconfig/fonts/LibreBaskerville-Regular.ttf',
+          bold: '/api/countryconfig/fonts/LibreBaskerville-Bold.ttf',
+          italics: '/api/countryconfig/fonts/LibreBaskerville-Italic.ttf',
+          bolditalics: '/api/countryconfig/fonts/LibreBaskerville-Regular.ttf'
         }
       }
     },
