18ca15b4e7877385cc2e0e0d4b734b091452eaae
tareq89 <tareqaziz.aust.cse@gmail.com>
2024-11-22 13:06:12 +0600

    marriage otherRelationship template operation changed to fieldValueTransformer

--- src/form/marriage/required-fields.ts
+++ src/form/marriage/required-fields.ts
@@ -89,7 +89,7 @@ export const witnessRelationshipForOthers = (
   ],
   mapping: {
     template: {
-      operation: 'selectTransformer',
+      operation: 'fieldValueTransformer',
       fieldName
     }
   }
