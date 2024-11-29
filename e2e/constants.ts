635fe6773a485dd0d971431dc2a3417ac157e890
tareq89 <tareqaziz.aust.cse@gmail.com>
2024-11-29 21:24:52 +0600

    in progress

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,8 +1,14 @@
+// export const DOMAIN = process.env.DOMAIN || 'farajaland-dev.opencrvs.org'
+// export const LOGIN_URL = 'https://login.' + DOMAIN
+// export const AUTH_URL = 'https://auth.' + DOMAIN
+// export const CLIENT_URL = 'https://register.' + DOMAIN
+// export const GATEWAY_HOST = 'https://gateway.' + DOMAIN
+
 export const DOMAIN = process.env.DOMAIN || 'farajaland-dev.opencrvs.org'
-export const LOGIN_URL = 'https://login.' + DOMAIN
-export const AUTH_URL = 'https://auth.' + DOMAIN
-export const CLIENT_URL = 'https://register.' + DOMAIN
-export const GATEWAY_HOST = 'https://gateway.' + DOMAIN
+export const LOGIN_URL = 'http://localhost:3020'
+export const AUTH_URL = 'http://localhost:4040'
+export const CLIENT_URL = 'http://localhost:3000'
+export const GATEWAY_HOST = 'http://localhost:7070'
 
 /*
  * This timeout is to ensure that all previous actions have been completed
