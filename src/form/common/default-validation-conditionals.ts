6a1108e5020fa5f95ef20a503185390c19ef62ec
Tameem Bin Haider <haidertameem@gmail.com>
2024-12-05 18:01:56 +0600

    chore: add missing import

--- src/form/common/default-validation-conditionals.ts
+++ src/form/common/default-validation-conditionals.ts
@@ -8,7 +8,7 @@
  *
  * Copyright (C) The OpenCRVS Authors located at https://github.com/opencrvs/opencrvs-core/blob/master/AUTHORS.
  */
-import { Conditional } from '../types/types'
+import { Conditional, IntegratingSystemType } from '../types/types'
 import { Validator } from '../types/validators'
 
 /**
