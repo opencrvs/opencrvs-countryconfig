6208844773455ef56bb223e6f17cd93e3b236f57
nifaliana <rija.nifaliana@gmail.com>
2024-12-20 10:51:34 +0300

    fix(nui): New generator implementation

--- src/api/nui/constants.ts
+++ src/api/nui/constants.ts
@@ -1,2 +1,6 @@
-export const NUI_GENERATOR_URL = process.env.NUI_GENERATOR_URL
+export const NUI_GENERATOR_URL =
+  'https://ss.operator.xroad.digital.gov.mg/r1/MG/GOV/UGD-MANAGEMENT/uin-federation/api'
 export const NUI_API_KEY = process.env.NUI_API_KEY
+export const X_ROAD_CLIENT = 'MG/GOV/UGD-MANAGEMENT/siecm'
+export const CLIENT_ID = 'siecm'
+export const CLIENT_SECRET = 'OA5acm9IREVZQS4Srsmy3m2O5p4TFQ7l'
