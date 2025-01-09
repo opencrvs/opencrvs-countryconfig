6daf1846d5619e9a413b194cbf3af92b5cc4793a
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2025-01-09 19:27:36 +0600

    chore: update form component types

--- src/form/types/types.ts
+++ src/form/types/types.ts
@@ -142,6 +142,10 @@ export const NID_VERIFICATION_BUTTON = 'NID_VERIFICATION_BUTTON'
 export const DIVIDER = 'DIVIDER'
 export const HEADING3 = 'HEADING3'
 export const SIGNATURE = 'SIGNATURE'
+export const LINK_BUTTON = 'LINK_BUTTON'
+export const ID_READER = 'ID_READER'
+export const HTTP = 'HTTP'
+export const ID_VERIFICATION_BANNER = 'ID_VERIFICATION_BANNER'
 
 export enum RadioSize {
   LARGE = 'large',
@@ -491,6 +495,51 @@ export interface ISignatureFormField extends IFormFieldBase {
   )[]
 }
 
+export interface IHttpFormField extends IFormFieldBase {
+  type: typeof HTTP
+  options: {
+    url: string
+    method?: string
+    headers: Record<string, string>
+    body?: Record<string, any>
+    params?: Record<string, any>
+  }
+}
+
+export interface IRedirectFormField extends IFormFieldBase {
+  type: typeof LINK_BUTTON
+  icon?: {
+    desktop: string
+    mobile: string
+  }
+  options: {
+    url: string
+    callback: {
+      trigger: string
+      params: Record<string, string>
+    }
+  }
+}
+
+export interface QRReaderType {
+  type: 'QR'
+}
+
+type ReaderType = QRReaderType | IRedirectFormField
+export interface IIDReaderFormField extends IFormFieldBase {
+  type: typeof ID_READER
+  dividerLabel: MessageDescriptor
+  manualInputInstructionLabel: MessageDescriptor
+  readers: [ReaderType, ...ReaderType[]]
+}
+
+export type BannerType = 'pending' | 'verified' | 'failed'
+export interface IBannerFormField extends IFormFieldBase {
+  type: typeof ID_VERIFICATION_BANNER
+  bannerType: BannerType
+  idFieldName: string
+}
+
 export type IFormField =
   | ITextFormField
   | ITelFormField
@@ -524,6 +573,10 @@ export type IFormField =
   | IDividerField
   | IHeading3Field
   | ISignatureFormField
+  | IIDReaderFormField
+  | IRedirectFormField
+  | IHttpFormField
+  | IBannerFormField
 
 export interface SelectComponentOption {
   value: string
