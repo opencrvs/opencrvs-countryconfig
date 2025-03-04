f511fb19aed717147170c6ad6b95f9b5dc3ca088
Your Name <you@example.com>
2025-03-04 18:15:00 +0200

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
-    ['playwright-ctrf-json-reporter', {}],
+    // ['playwright-ctrf-json-reporter', {}],
+    ['@estruyf/github-actions-reporter'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
