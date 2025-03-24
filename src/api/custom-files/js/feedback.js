7838c238c884f2d2862a35f1c019459553f3fea8
t.randrianarisoa <t.randrianarisoa@etechconsulting-mg.com>
2025-03-24 16:52:37 +0300

    change feedback language based on app language

--- src/api/custom-files/js/feedback.js
+++ src/api/custom-files/js/feedback.js
@@ -1,5 +1,43 @@
-window.appziSettings = {
-    data: {
-        'lang': 'mg',
-    }
+"use strict";
+function getLocaleFromIndexedDB(dbName, storeName, key) {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(dbName);
+
+        request.onerror = () => reject("Failed to open IndexedDB");
+
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            const transaction = db.transaction(storeName, "readonly");
+            const store = transaction.objectStore(storeName);
+            const getRequest = store.get(key);
+
+            getRequest.onsuccess = () => resolve(getRequest.result || "en");
+            getRequest.onerror = () => reject("Failed to fetch locale from IndexedDB");
+        };
+    });
 }
+
+getLocaleFromIndexedDB("OpenCRVS", "keyvaluepairs", "language")
+    .then((locale) => {
+        console.warn("getLocaleFromIndexedDB Current Locale:", locale);
+        window.appziSettings = {
+            data: {
+                'lang': ['fr', 'mg'].includes(locale) ? locale : 'fr'
+            }
+        }
+    })
+    .catch((error) => console.error(error));
+
+/**
+ * Listen for changes to the locale in IndexedDB
+ */
+window.addEventListener('storagesetitem', (event) => {
+    console.warn("storagesetitem :", event?.detail);
+    if (event?.detail?.key == 'language') {
+        window.appziSettings = {
+            data: {
+                'lang': ['fr', 'mg'].includes(event?.detail?.value) ? event?.detail?.value : 'fr'
+            }
+        }
+    }
+});
