f7dc3cff2f1b40921b0eb2be3e6419bc6f5185d0
Keksike <cihan.m.bebek@gmail.com>
2025-03-25 15:51:32 +0200

    add third print-certificate test file

---
+++ e2e/testcases/v2-print-certificate/birth/36.04-validate-certify-record-page.spec.ts
@@ -0,0 +1,48 @@
+import { expect, test, type Page } from '@playwright/test'
+import { CREDENTIALS } from '../../../constants'
+import { getToken, loginToV2 } from '../../../helpers'
+import {
+  CreateDeclarationResponse,
+  createDeclaration
+} from './data/birth-declaration'
+
+test.describe.serial('4.0 Validate "Certify record" page', () => {
+  let declaration: CreateDeclarationResponse
+  let page: Page
+
+  test.beforeAll(async ({ browser }) => {
+    const token = await getToken(
+      CREDENTIALS.LOCAL_REGISTRAR.USERNAME,
+      CREDENTIALS.LOCAL_REGISTRAR.PASSWORD
+    )
+    declaration = await createDeclaration(token)
+    page = await browser.newPage()
+    await loginToV2(page)
+  })
+
+  test.afterAll(async () => {
+    await page.close()
+  })
+
+  test('4.1 continue with print in advance redirect to review page', async () => {
+    const childName = `${declaration.data['child.firstname']} ${declaration.data['child.surname']}`
+    await page.getByRole('button', { name: childName }).click()
+    await page.getByRole('button', { name: 'Action' }).click()
+    await page.getByRole('listitem').click()
+
+    await page.locator('#certificateTemplateId svg').click()
+    await page.getByText('Birth Certificate').nth(1).click()
+
+    await page.locator('#collector____requesterId div').nth(4).click()
+    await page.getByText('Print in advance', { exact: true }).click()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(
+      page
+        .url()
+        .includes(
+          `/print-certificate/${declaration.eventId}/review?templateId=v2.birth-certificate`
+        )
+    ).toBeTruthy()
+  })
+})
