97ccb6d0ecab3cad14defacd38bc9351cc9f6b14
Keksike <cihan.m.bebek@gmail.com>
2025-03-28 09:50:31 +0200

    progress

--- src/api/custom-event/handler.ts
+++ src/api/custom-event/handler.ts
@@ -15,34 +15,20 @@ import { logger } from '@countryconfig/logger'
 import * as Hapi from '@hapi/hapi'
 import { createClient } from '@opencrvs/toolkit/api'
 import { EventDocument } from '@opencrvs/toolkit/events'
+import uuid from 'uuid'
+import { customAlphabet } from 'nanoid'
+
+const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 12)
+
+function generateRegistrationNumber() {
+  return nanoid()
+}
 
 export function getCustomEventsHandler(
-  request: Hapi.Request,
+  _: Hapi.Request,
   h: Hapi.ResponseToolkit
 ) {
   return h.response([tennisClubMembershipEvent, birthEvent]).code(200)
-}
-
-export async function onRegisterHandler(
-  request: Hapi.Request,
-  h: Hapi.ResponseToolkit
-) {
-  const event = EventDocument.parse(request.payload)
-  const client = createClient(
-    env.GATEWAY_URL + '/events',
-    request.headers.authorization
-  )
-
-  logger.info(`Confirming registration ${event.id}`)
-  await client.event.registration.confirm.mutate({
-    eventId: event.id,
-    data: {
-      status: 'CONFIRMED'
-    }
-  })
-  logger.info(`Registration ${event.id} confirmed`)
-
-  return h.response().code(200)
 }
 
 export function onAnyActionHandler(
@@ -52,5 +38,33 @@ export function onAnyActionHandler(
   // This catch-all event route can receive either legacy FHIR events with `Content-Type: application/fhir+json` or new events with `Content-Type: application/json`
   console.log(request.params.event, request.params.action)
   console.log(request.payload)
+  console.log('RES CIHAN2', generateRegistrationNumber())
+  console.log('RES CIHAN2', generateRegistrationNumber())
+  console.log('RES CIHAN2', generateRegistrationNumber())
   return h.response().code(200)
+}
+
+export async function onRegisterHandler(
+  _: Hapi.Request,
+  h: Hapi.ResponseToolkit
+) {
+  // const event = EventDocument.parse(request.payload)
+  // const client = createClient(
+  //   env.GATEWAY_URL + '/events',
+  //   request.headers.authorization
+  // )
+
+  // logger.info(`Confirming registration ${event.id}`)
+  // await client.event.actions.register.accept.mutate({
+  //   transactionId: uuid.v4(),
+  //   eventId: event.id,
+  //   data: {
+  //     status: 'CONFIRMED'
+  //   }
+  // })
+  // logger.info(`Registration ${event.id} confirmed`)
+
+  return h
+    .response({ registrationNumber: generateRegistrationNumber() })
+    .code(200)
 }
--- src/index.ts
+++ src/index.ts
@@ -68,6 +68,7 @@ import {
   onRegisterHandler
 } from '@countryconfig/api/custom-event/handler'
 import { readFileSync } from 'fs'
+import { ActionType } from '@opencrvs/toolkit/events'
 
 export interface ITokenPayload {
   sub: string
@@ -565,18 +566,8 @@ export async function createServer() {
     path: '/events',
     handler: getCustomEventsHandler,
     options: {
-      tags: ['api', 'custom-event'],
+      tags: ['api', 'events'],
       description: 'Serves custom events'
-    }
-  })
-
-  server.route({
-    method: 'POST',
-    path: '/events/TENNIS_CLUB_MEMBERSHIP/actions/REGISTER',
-    handler: onRegisterHandler,
-    options: {
-      tags: ['api', 'custom-event'],
-      description: 'Receives notifications on event actions'
     }
   })
 
@@ -585,7 +576,18 @@ export async function createServer() {
     path: '/events/{event}/actions/{action}',
     handler: onAnyActionHandler,
     options: {
-      tags: ['api', 'custom-event'],
+      tags: ['api', 'events'],
+      description: 'Receives notifications on event actions'
+    }
+  })
+
+  server.route({
+    method: 'POST',
+    // TODO CIHAN: can we get this from the event config and action config?
+    path: `/events/tennis-club-membership/actions/${ActionType.REGISTER}`,
+    handler: onRegisterHandler,
+    options: {
+      tags: ['api', 'events'],
       description: 'Receives notifications on event actions'
     }
   })
