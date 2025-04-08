9efadcfa238d3d95b2ddd311d67d0519d547a831
Markus <markus@agilepursuit.fi>
2025-04-08 17:36:33 +0300

    fix: update location name

--- e2e/testcases/birth/declarations/birth-declaration-1.spec.ts
+++ e2e/testcases/birth/declarations/birth-declaration-1.spec.ts
@@ -32,7 +32,8 @@ test.describe.serial('1. Birth declaration case - 1', () => {
     birthType: 'Single',
     weightAtBirth: 2.4,
     placeOfBirth: 'Health Institution',
-    birthLocation: 'Golden Valley Rural Health Centre',
+    birthLocation:
+      'Golden Valley Rural Health Centre, Ibombo, Central, Farajaland',
     informantType: 'Mother',
     informantEmail: faker.internet.email(),
     mother: {
