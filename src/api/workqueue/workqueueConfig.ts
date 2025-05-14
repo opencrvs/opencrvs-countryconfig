0a582432728dda5fc31671afbc850be59786a6cf
jamil314 <jamil31415926@gmail.com>
2025-05-14 20:29:38 +0600

    feat: add workqueues

--- src/api/workqueue/workqueueConfig.ts
+++ src/api/workqueue/workqueueConfig.ts
@@ -76,5 +76,193 @@ export const Workqueues = defineWorkqueue([
     query: {
       status: { type: 'exact', term: 'WAITING_FOR_ATTESTATION' }
     }
+  },
+
+  {
+    slug: 'assigned-to-you',
+    name: {
+      id: 'workqueues.assignedToYou.title',
+      defaultMessage: 'Assigned to you',
+      description: 'Title of assigned to you workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'ASSIGNED_TO_YOU' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'recent',
+    name: {
+      id: 'workqueues.recent.title',
+      defaultMessage: 'Recent',
+      description: 'Title of recent workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'RECENT' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'notifications',
+    name: {
+      id: 'workqueues.notifications.title',
+      defaultMessage: 'Notifications',
+      description: 'Title of notifications workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'REQUIRES_COMPLETION' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'sent-for-review',
+    name: {
+      id: 'workqueues.sentForReview.title',
+      defaultMessage: 'Sent for review',
+      description: 'Title of sent for review workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'SENT_FOR_REVIEW' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'in-review',
+    name: {
+      id: 'workqueues.inReview.title',
+      defaultMessage: 'Ready for review',
+      description: 'Title of ready for review workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'IN_REVIEW' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'in-review-all',
+    name: {
+      id: 'workqueues.inReviewAll.title',
+      defaultMessage: 'Ready for review',
+      description: 'Title of ready for review (all) workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'IN_REVIEW_ALL' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'requires-updates',
+    name: {
+      id: 'workqueues.requiresUpdates.title',
+      defaultMessage: 'Requires updates',
+      description: 'Title of requires updates workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'REQUIRES_UPDATES' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'sent-for-approval',
+    name: {
+      id: 'workqueues.sentForApproval.title',
+      defaultMessage: 'Sent for approval',
+      description: 'Title of sent for approval workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'SENT_FOR_APPROVAL' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'in-external-validation',
+    name: {
+      id: 'workqueues.inExternalValidation.title',
+      defaultMessage: 'In external validation',
+      description: 'Title of in external validation workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'IN_EXTERNAL_VALIDATION' }
+    },
+    actions: [
+      {
+        type: 'REVIEW',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'ready-to-print',
+    name: {
+      id: 'workqueues.readyToPrint.title',
+      defaultMessage: 'Ready to print',
+      description: 'Title of ready to print workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'READY_TO_PRINT' }
+    },
+    actions: [
+      {
+        type: 'PRINT',
+        conditionals: []
+      }
+    ]
+  },
+  {
+    slug: 'ready-to-issue',
+    name: {
+      id: 'workqueues.readyToIssue.title',
+      defaultMessage: 'Ready to issue',
+      description: 'Title of ready to issue workqueue'
+    },
+    query: {
+      status: { type: 'exact', term: 'READY_TO_ISSUE' }
+    },
+    actions: [
+      {
+        type: 'ISSUE',
+        conditionals: []
+      }
+    ]
   }
 ])
