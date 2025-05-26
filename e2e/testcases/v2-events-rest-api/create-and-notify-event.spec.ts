06a24d7ed3faaa9445990f518f062b2c5f20ad3d
cibelius <cihan.m.bebek@gmail.com>
2025-05-26 14:17:49 +0300

    create and use system user

--- e2e/testcases/v2-events-rest-api/create-and-notify-event.spec.ts
+++ e2e/testcases/v2-events-rest-api/create-and-notify-event.spec.ts
@@ -1,16 +1,15 @@
 import { expect, test } from '@playwright/test'
 import { v4 as uuidv4 } from 'uuid'
-import { CLIENT_URL } from '../../constants'
+import { CLIENT_URL, GATEWAY_HOST } from '../../constants'
 import { CREDENTIALS } from '../../constants'
-import { getToken } from '../../helpers'
+import { getClientToken, getToken } from '../../helpers'
 
 async function fetchClientAPI(
   path: string,
   method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-  body: object = {},
-  credentials = CREDENTIALS.LOCAL_REGISTRAR
+  token: string,
+  body: object = {}
 ) {
-  const token = await getToken(credentials.USERNAME, credentials.PASSWORD)
   const url = new URL(`${CLIENT_URL}${path}`)
 
   return fetch(url, {
@@ -23,61 +22,134 @@ async function fetchClientAPI(
   })
 }
 
+async function createSystemUser() {
+  const token = await getToken(
+    CREDENTIALS.NATIONAL_SYSTEM_ADMIN.USERNAME,
+    CREDENTIALS.NATIONAL_SYSTEM_ADMIN.PASSWORD
+  )
+
+  const res = await fetch(`${GATEWAY_HOST}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      query: `
+      mutation registerSystem($system: SystemInput) {
+        registerSystem(system: $system) {
+          clientSecret
+          system {
+            clientId
+            shaSecret
+          }
+        }
+      }
+    `,
+      variables: {
+        system: { name: `e2e-test-${uuidv4()}`, type: 'HEALTH' }
+      }
+    })
+  })
+
+  const body = await res.json()
+
+  return {
+    clientSecret: body.data.registerSystem.clientSecret as string,
+    system: body.data.registerSystem.system as {
+      clientId: string
+      shaSecret: string
+    }
+  }
+}
+
 const EVENT_TYPE = 'v2.birth'
 
-// TODO CIHAN: login with j.campbell and create a system user?
-// TODO CIHAN: use system user
+test.describe('Events REST API', () => {
+  let clientToken: string
 
-test.describe('POST /api/events/events/create', () => {
-  test.skip('returns HTTP 403 when user is missing scope', async () => {
-    const response = await fetchClientAPI('/api/events/events/create', 'POST')
-
-    expect(response.status).toBe(403)
+  test.beforeAll(async () => {
+    const { system, clientSecret } = await createSystemUser()
+    clientToken = await getClientToken(system.clientId, clientSecret)
   })
 
-  test('returns HTTP 400 with missing payload', async () => {
-    const response = await fetchClientAPI('/api/events/events/create', 'POST')
-    expect(response.status).toBe(400)
-  })
+  test.describe('POST /api/events/events', () => {
+    test.skip('returns HTTP 403 when user is missing scope', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events',
+        'POST',
+        clientToken
+      )
 
-  test('returns HTTP 200 with valid payload', async () => {
-    const response = await fetchClientAPI('/api/events/events/create', 'POST', {
-      type: EVENT_TYPE,
-      transactionId: uuidv4()
+      expect(response.status).toBe(403)
     })
 
-    const body = await response.json()
-
-    expect(response.status).toBe(200)
-    expect(body.type).toBe(EVENT_TYPE)
-    expect(body.actions.length).toBe(2)
-  })
-})
-
-test.describe('POST /api/events/events/notify', () => {
-  test('returns HTTP 403 when user is missing scope', async () => {
-    const response = await fetchClientAPI('/api/events/events/notify', 'POST')
-    expect(response.status).toBe(403)
-  })
-
-  test('returns HTTP 400 with missing payload', async () => {
-    const response = await fetchClientAPI('/api/events/events/notify', 'POST')
-
-    expect(response.status).toBe(400)
-  })
-
-  test('returns HTTP 200 with valid payload', async () => {
-    const response = await fetchClientAPI('/api/events/events/notify', 'POST', {
-      type: EVENT_TYPE,
-      transactionId: uuidv4()
+    test('returns HTTP 400 with missing payload', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events',
+        'POST',
+        clientToken
+      )
+      expect(response.status).toBe(400)
     })
 
-    const body = await response.json()
+    test('returns HTTP 200 with valid payload', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events',
+        'POST',
+        clientToken,
+        {
+          type: EVENT_TYPE,
+          transactionId: uuidv4()
+        }
+      )
 
-    expect(response.status).toBe(200)
-    expect(body.type).toBe(EVENT_TYPE)
-    expect(body.actions.length).toBe(2)
+      const body = await response.json()
 
-    // TODO CIHAN: check from UI that event is created
+      expect(response.status).toBe(200)
+      expect(body.type).toBe(EVENT_TYPE)
+      expect(body.actions.length).toBe(2)
+    })
+  })
+
+  test.describe('POST /api/events/events/notifications', () => {
+    test('returns HTTP 403 when user is missing scope', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events/notifications',
+        'POST',
+        clientToken
+      )
+      expect(response.status).toBe(403)
+    })
+
+    test('returns HTTP 400 with missing payload', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events/notifications',
+        'POST',
+        clientToken
+      )
+
+      expect(response.status).toBe(400)
+    })
+
+    test('returns HTTP 200 with valid payload', async () => {
+      const response = await fetchClientAPI(
+        '/api/events/events/notifications',
+        'POST',
+        clientToken,
+        {
+          type: EVENT_TYPE,
+          transactionId: uuidv4()
+        }
+      )
+
+      const body = await response.json()
+
+      expect(response.status).toBe(200)
+      expect(body.type).toBe(EVENT_TYPE)
+      expect(body.actions.length).toBe(2)
+
+      // TODO CIHAN: check from UI that event is created
+    })
   })
 })
