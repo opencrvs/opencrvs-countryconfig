f8b3e47fc1a18326c2f4c6e52dcb09fca587d853
cibelius <cihan.m.bebek@gmail.com>
2025-07-01 15:11:46 +0300

    add testcase

---
+++ e2e/testcases/v2-correction-birth/birth-correction-flow.spec.ts
@@ -0,0 +1,172 @@
+import { expect, test, type Page } from '@playwright/test'
+import { getToken, loginToV2 } from '../../helpers'
+import { faker } from '@faker-js/faker'
+import { CREDENTIALS } from '../../constants'
+import {
+  createDeclaration,
+  Declaration
+} from '../v2-test-data/birth-declaration'
+import { ensureAssigned, expectInUrl, selectAction } from '../../v2-utils'
+import { formatV2ChildName } from '../v2-birth/helpers'
+
+test.describe.serial('Birth correction flow', () => {
+  let declaration: Declaration
+  let eventId: string
+  let page: Page
+
+  test.beforeAll(async ({ browser }) => {
+    const token = await getToken(
+      CREDENTIALS.LOCAL_REGISTRAR.USERNAME,
+      CREDENTIALS.LOCAL_REGISTRAR.PASSWORD
+    )
+    const res = await createDeclaration(
+      token,
+      undefined,
+      undefined,
+      'HEALTH_FACILITY'
+    )
+    declaration = res.declaration
+    eventId = res.eventId
+
+    page = await browser.newPage()
+  })
+
+  test('Navigate to the correction form', async () => {
+    await loginToV2(page, CREDENTIALS.REGISTRATION_AGENT)
+    await page.getByRole('button', { name: 'Ready to print' }).click()
+    await page
+      .getByRole('button', { name: formatV2ChildName(declaration) })
+      .click()
+    await ensureAssigned(page)
+    await selectAction(page, 'Correct record')
+  })
+
+  test('Try to continue without filling in required fields', async () => {
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(page.locator('#requester____type_error')).toBeVisible()
+    await expect(page.locator('#reason____option_error')).toBeVisible()
+  })
+
+  test('Fill in the correction details form', async () => {
+    await page.locator('#requester____type').click()
+    await page.getByText('Informant (Mother)', { exact: true }).click()
+
+    await page.locator('#reason____option').click()
+    await page
+      .getByText('Myself or an agent made a mistake (Clerical error)', {
+        exact: true
+      })
+      .click()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await page.getByRole('button', { name: 'Verified' }).click()
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await page
+      .locator('#fees____amount')
+      .fill(faker.number.int({ min: 1, max: 1000 }).toString())
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+  })
+
+  test('Review page should be displayed and continue button should be disabled', async () => {
+    await expectInUrl(page, `/events/request-correction/${eventId}/review`)
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeDisabled()
+  })
+
+  test('Go through the declaration correction form without changing any details', async () => {
+    await page
+      .getByRole('button', { name: 'Change all', exact: true })
+      .first()
+      .click()
+
+    await expect(
+      page.getByRole('button', { name: 'Back to review' })
+    ).toBeEnabled()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(
+      page.getByRole('button', { name: 'Back to review' })
+    ).toBeEnabled()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(
+      page.getByRole('button', { name: 'Back to review' })
+    ).toBeEnabled()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expect(
+      page.getByRole('button', { name: 'Back to review' })
+    ).toBeEnabled()
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+  })
+
+  test('When back on review page, continue button should still be disabled', async () => {
+    await expectInUrl(page, `/events/request-correction/${eventId}/review`)
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeDisabled()
+  })
+
+  test('After changing a value, continue button should be enabled', async () => {
+    await page.getByTestId('change-button-informant.email').click()
+
+    await page
+      .getByTestId('text__informant____email')
+      .fill(faker.internet.email())
+
+    await page.getByRole('button', { name: 'Back to review' }).click()
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeEnabled()
+  })
+
+  test('After changing the value back to the original, continue button should be disabled', async () => {
+    await page.getByTestId('change-button-informant.email').click()
+
+    await page
+      .getByTestId('text__informant____email')
+      .fill(declaration['informant.email'])
+
+    await page.getByRole('button', { name: 'Back to review' }).click()
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeDisabled()
+  })
+
+  test('After changing another value to an invalid value, continue button should still be disabled', async () => {
+    await page.getByTestId('change-button-child.dob').click()
+    // Future date
+    await page.getByTestId('child____dob-yyyy').fill('2045')
+    await page.getByRole('button', { name: 'Back to review' }).click()
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeDisabled()
+    await expect(page.getByText('Must be a valid birth date')).toBeVisible()
+  })
+
+  test('After changing the value to a valid value, continue button should be enabled', async () => {
+    await page.getByTestId('change-button-child.dob').click()
+    await page.getByTestId('child____dob-yyyy').fill('2024')
+    await page.getByTestId('child____dob-mm').fill('6')
+    await page.getByTestId('child____dob-dd').fill('24')
+    await page
+      .getByTestId('text__child____reason')
+      .fill(faker.lorem.sentence(4))
+    await page.getByRole('button', { name: 'Back to review' }).click()
+    await expect(page.getByRole('button', { name: 'Continue' })).toBeEnabled()
+  })
+
+  test('Continue to the summary page', async () => {
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await expectInUrl(page, `/events/request-correction/${eventId}/summary`)
+    await expect(
+      page.getByRole('button', { name: 'Back to review' })
+    ).toBeEnabled()
+    await expect(
+      page.getByRole('button', { name: 'Submit correction request' })
+    ).toBeEnabled()
+  })
+
+  test('Submit correction request', async () => {
+    await page
+      .getByRole('button', { name: 'Submit correction request' })
+      .click()
+  })
+
+  // @TODO: check that correction request appears correctly in workqueues and can be accepted/declined
+  test.skip('Submit correction request', async () => {})
+})
