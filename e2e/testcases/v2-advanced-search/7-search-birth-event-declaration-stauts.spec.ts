332a9bf9a148d7cd3f09445cf51891e55c9527ca
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-07-02 20:00:33 +0600

    fix: remove unused import

--- e2e/testcases/v2-advanced-search/7-search-birth-event-declaration-stauts.spec.ts
+++ e2e/testcases/v2-advanced-search/7-search-birth-event-declaration-stauts.spec.ts
@@ -1,10 +1,6 @@
 import { expect, test, type Page } from '@playwright/test'
 import { loginToV2 } from '../../helpers'
-// import { createDeclaration } from '../v2-test-data/birth-declaration-with-father-brother'
-// import { CREDENTIALS } from '../../constants'
 import { faker } from '@faker-js/faker'
-// import { formatDateToLongString } from './utils'
-// import { getMonthFormatted } from './helper'
 
 test.describe
   .serial("Advanced Search - Birth Event Declaration - Informant's details", () => {
