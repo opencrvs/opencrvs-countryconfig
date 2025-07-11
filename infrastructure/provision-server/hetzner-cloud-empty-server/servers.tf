c14dc02813d51321d9a1235dc0f41ea6c7c66009
Vadym Mudryi <vadym.mudryi@gmail.com>
2025-07-11 13:03:10 +0300

    fix: Add secrets and change hetzner provision location

--- infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
+++ infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
@@ -4,7 +4,7 @@ module "opencrvs_single_node" {
   count       = (var.env_type == "single-node") ? 1 : 0
   source      = "./single-node"
   name        = "${var.country_name}-${var.env_name}"
-  location    = "fsn1"
+  location    = "hil"
   server_type = "cpx41"
   user_data = templatefile("cloud-init.yaml", {
     ssh_key = trimspace(file(".ssh/id_rsa.pub"))
@@ -17,7 +17,7 @@ module "opencrvs_multi_node" {
   source       = "./multi-node"
   country_name = var.country_name
   env_name     = var.env_name
-  location     = "fsn1"
+  location     = "hil"
   server_type  = "cpx41"
   user_data = templatefile("cloud-init.yaml", {
     ssh_key = trimspace(file(".ssh/id_rsa.pub"))
