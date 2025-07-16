89c6305f48fb4b3a29c40138336ed1aafbf8e47d
Riku Rouvila <riku.rouvila@gmail.com>
2025-07-16 17:00:49 +0300

    make it possible to enable v2 by default also in prod environments

--- src/client-config.prod.js
+++ src/client-config.prod.js
@@ -24,7 +24,7 @@ window.config = {
   MINIO_BASE_URL: minioBaseUrl.startsWith('http')
     ? minioBaseUrl
     : // Resolve the protocol on the same level as the other URLs.
-      `${scheme}${minioBaseUrl}`, // URL without path/bucket information, used for file uploads, v2
+    `${scheme}${minioBaseUrl}`, // URL without path/bucket information, used for file uploads, v2
   /** Bucket name is hardcoded as 'ocrvs'. In live system, it cannot be changed without data migration.
    * In E2E environment, buckets are separated from different PR environments to avoid conflicts. @see start-prod.sh
    */
@@ -37,5 +37,8 @@ window.config = {
   LEADERBOARDS_DASHBOARD_URL: `${scheme}//metabase.${hostname}/public/dashboard/acae0527-74be-4804-a3ee-f8b3c9c8784c#bordered=false&titled=false&refresh=300`,
   REGISTRATIONS_DASHBOARD_URL: `${scheme}//metabase.${hostname}/public/dashboard/fec78656-e4f9-4b51-b540-0fed81dbd821#bordered=false&titled=false&refresh=300`,
   STATISTICS_DASHBOARD_URL: `${scheme}//metabase.${hostname}/public/dashboard/a17e9bc0-15a2-4bd1-92fa-ab0f346227ca#bordered=false&titled=false&refresh=300`,
-  FEATURES: {}
+  FEATURES: {
+    // The V2_EVENTS variable is passed down from src/index.ts:309
+    V2_EVENTS: {{ V2_EVENTS }}
+  }
 }
