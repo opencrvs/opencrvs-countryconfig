baa10c6a9afa00d24f2b507797280950c6b59452
jamil314 <jamil31415926@gmail.com>
2025-07-21 13:41:25 +0600

    e2e: only run v2 e2es in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './e2e/testcases',
+  testMatch: ['**/v2-*/**/*.spec.ts'], // Run only v2 tests
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
