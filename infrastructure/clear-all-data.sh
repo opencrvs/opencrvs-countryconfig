087cfb7c95a05a40035d6e6e5baffcb421676f40
cibelius <cihan.m.bebek@gmail.com>
2025-09-12 11:12:28 +0300

    run migrations service

--- infrastructure/clear-all-data.sh
+++ infrastructure/clear-all-data.sh
@@ -154,3 +154,8 @@ echo "✅ Database and roles dropped."
 echo "🚀 Reinitializing Postgres with on-deploy.sh..."
 
 docker service update --force opencrvs_postgres-on-update
+
+# Rerun the migrations service
+#-----------------------------
+docker service scale opencrvs_migrations=0
+docker service scale opencrvs_migrations=1
