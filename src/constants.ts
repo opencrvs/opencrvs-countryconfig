72f0a4119eafe242b109caadd75b7cc965b106fa
Tameem Bin Haider <haidertameem@gmail.com>
2025-09-19 11:38:37 +0600

    chore: parse V2_EVENTS via envalid

--- src/constants.ts
+++ src/constants.ts
@@ -31,4 +31,5 @@ export const CHECK_INVALID_TOKEN = env.CHECK_INVALID_TOKEN
 
 export const PRODUCTION = env.isProd
 export const QA_ENV = env.QA_ENV
+export const V2_EVENTS = env.V2_EVENTS
 export const ANALYTICS_DATABASE_URL = env.ANALYTICS_DATABASE_URL
