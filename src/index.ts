6597c0789d936a4b6c98bde3446cf094c17e777a
cibelius <cihan.m.bebek@gmail.com>
2025-09-25 15:31:08 +0300

    add shortcircuit if no analytics env configured

--- src/index.ts
+++ src/index.ts
@@ -74,6 +74,7 @@ import {
   syncLocationStatistics
 } from './analytics/analytics'
 import { getClient } from './analytics/postgres'
+import { env } from './environment'
 
 export interface ITokenPayload {
   sub: string
@@ -573,6 +574,18 @@ export async function createServer() {
       }
     },
     handler: async (req, h) => {
+      if (!env.ANALYTICS_DATABASE_URL) {
+        // kill client upload immediately
+        if (!req.raw.req.destroyed) {
+          req.raw.req.destroy()
+        }
+
+        logger.warn(
+          'Skipping reindex, no ANALYTICS_DATABASE_URL environment variable set.'
+        )
+        return h.response().code(200)
+      }
+
       const stream = req.raw.req.pipe(StreamArray.withParser())
       const BATCH_SIZE = 1000
       const queue: EventDocument[] = []
