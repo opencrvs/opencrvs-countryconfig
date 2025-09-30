831be804fc71246f8fb532d5ecd43db767a7c317
jamil314 <jamil31415926@gmail.com>
2025-09-30 17:53:01 +0600

    fix: url in e2e

--- e2e/testcases/v2-messages/roles-in-record-audit.spec.ts
+++ e2e/testcases/v2-messages/roles-in-record-audit.spec.ts
@@ -1,113 +1,66 @@
 import { expect, test } from '@playwright/test'
 import { CLIENT_V2_URL, CREDENTIALS } from '../../constants'
-import { getToken, joinValuesWith, loginToV2 } from '../../helpers'
+import { auditRecord, getToken, loginToV2 } from '../../helpers'
 import { createDeclaration } from '../v2-test-data/birth-declaration'
 import { ActionType } from '@opencrvs/toolkit/events'
 import { ensureAssigned } from '../../v2-utils'
 import { formatV2ChildName } from '../v2-birth/helpers'
 
+const testCases = [
+  {
+    credential: CREDENTIALS.FIELD_AGENT,
+    action: ActionType.DECLARE,
+    expectedAuditRole: 'Hospital Clerk'
+  },
+
+  {
+    credential: CREDENTIALS.ANOTHER_FIELD_AGENT,
+    action: ActionType.DECLARE,
+    expectedAuditRole: 'Community Leader'
+  },
+  {
+    credential: CREDENTIALS.REGISTRATION_AGENT,
+    action: ActionType.VALIDATE,
+    expectedAuditRole: 'Registration Agent'
+  },
+  {
+    credential: CREDENTIALS.LOCAL_REGISTRAR,
+    action: ActionType.REGISTER,
+    expectedAuditRole: 'Local Registrar'
+  }
+]
+
 test.describe('Roles in Record Audit', () => {
-  test('Hospital Clerk (Field Agent)', async ({ page }) => {
-    const token = await getToken(
-      CREDENTIALS.FIELD_AGENT.USERNAME,
-      CREDENTIALS.FIELD_AGENT.PASSWORD
-    )
-    const res = await createDeclaration(token, undefined, ActionType.DECLARE)
-    const eventId = res.eventId
+  for (const { credential, expectedAuditRole, action } of testCases) {
+    test(expectedAuditRole, async ({ browser }) => {
+      const page = await browser.newPage()
+      const token = await getToken(credential.USERNAME, credential.PASSWORD)
+      const res = await createDeclaration(token, undefined, action)
 
-    await loginToV2(page, CREDENTIALS.LOCAL_REGISTRAR)
-    await expect(page.locator('#content-name')).toHaveText('Assigned to you', {
-      timeout: 90000
+      await loginToV2(page, CREDENTIALS.LOCAL_REGISTRAR)
+
+      await expect(page.locator('#content-name')).toHaveText(
+        'Assigned to you',
+        {
+          timeout: 90000
+        }
+      )
+
+      await page
+        .getByRole('textbox', { name: 'Search for a tracking ID' })
+        .fill(formatV2ChildName(res.declaration))
+
+      await page.getByRole('button', { name: 'Search' }).click()
+      await page
+        .getByRole('button', {
+          name: formatV2ChildName(res.declaration),
+          exact: true
+        })
+        .click()
+
+      await ensureAssigned(page)
+
+      await expect(page.locator('#row_0')).toContainText(expectedAuditRole)
     })
-    await page.goto(
-      joinValuesWith([CLIENT_V2_URL, 'events', 'overview', eventId], '/')
-    )
-    await expect(page.locator('#content-name')).toHaveText(
-      formatV2ChildName(res.declaration),
-      {
-        timeout: 90000
-      }
-    )
-
-    await ensureAssigned(page)
-    await expect(page.locator('#row_0')).toContainText('Hospital Clerk')
-  })
-
-  test('Community Leader (Another Field Agent)', async ({ page }) => {
-    const token = await getToken(
-      CREDENTIALS.ANOTHER_FIELD_AGENT.USERNAME,
-      CREDENTIALS.ANOTHER_FIELD_AGENT.PASSWORD
-    )
-    const res = await createDeclaration(token, undefined, ActionType.DECLARE)
-    const eventId = res.eventId
-
-    await loginToV2(page, CREDENTIALS.LOCAL_REGISTRAR)
-    await expect(page.locator('#content-name')).toHaveText('Assigned to you', {
-      timeout: 90000
-    })
-    await page.goto(
-      joinValuesWith([CLIENT_V2_URL, 'events', 'overview', eventId], '/')
-    )
-    await expect(page.locator('#content-name')).toHaveText(
-      formatV2ChildName(res.declaration),
-      {
-        timeout: 90000
-      }
-    )
-
-    await ensureAssigned(page)
-    await expect(page.locator('#row_0')).toContainText('Community Leader')
-  })
-
-  test('Registration Agent', async ({ page }) => {
-    const token = await getToken(
-      CREDENTIALS.REGISTRATION_AGENT.USERNAME,
-      CREDENTIALS.REGISTRATION_AGENT.PASSWORD
-    )
-    const res = await createDeclaration(token, undefined, ActionType.VALIDATE)
-    const eventId = res.eventId
-
-    await loginToV2(page, CREDENTIALS.LOCAL_REGISTRAR)
-    await expect(page.locator('#content-name')).toHaveText('Assigned to you', {
-      timeout: 90000
-    })
-    await page.goto(
-      joinValuesWith([CLIENT_V2_URL, 'events', 'overview', eventId], '/')
-    )
-    await expect(page.locator('#content-name')).toHaveText(
-      formatV2ChildName(res.declaration),
-      {
-        timeout: 90000
-      }
-    )
-
-    await ensureAssigned(page)
-    await expect(page.locator('#row_0')).toContainText('Registration Agent')
-  })
-
-  test('Local Registrar', async ({ page }) => {
-    const token = await getToken(
-      CREDENTIALS.LOCAL_REGISTRAR.USERNAME,
-      CREDENTIALS.LOCAL_REGISTRAR.PASSWORD
-    )
-    const res = await createDeclaration(token, undefined, ActionType.REGISTER)
-    const eventId = res.eventId
-
-    await loginToV2(page, CREDENTIALS.LOCAL_REGISTRAR)
-    await expect(page.locator('#content-name')).toHaveText('Assigned to you', {
-      timeout: 90000
-    })
-    await page.goto(
-      joinValuesWith([CLIENT_V2_URL, 'events', 'overview', eventId], '/')
-    )
-    await expect(page.locator('#content-name')).toHaveText(
-      formatV2ChildName(res.declaration),
-      {
-        timeout: 90000
-      }
-    )
-
-    await ensureAssigned(page)
-    await expect(page.locator('#row_0')).toContainText('Local Registrar')
-  })
+  }
 })
