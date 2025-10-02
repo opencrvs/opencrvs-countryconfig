65b76233245a60cae2ece00d01831caf7cb5955d
cibelius <cihan.m.bebek@gmail.com>
2025-10-02 15:36:02 +0300

    improve tests

--- e2e/testcases/v2-print-certificate/death/validate-pdf-details.spec.ts
+++ e2e/testcases/v2-print-certificate/death/validate-pdf-details.spec.ts
@@ -11,6 +11,58 @@ import {
   selectRequesterType
 } from '../death/helpers'
 
+async function expectInPdf(page: Page, text: string) {
+  await expect(page.locator('#print')).toContainText(text)
+}
+
+test.describe.serial("Validate 'Death Certificate' PDF details", () => {
+  let page: Page
+  let declaration: Declaration
+
+  test.beforeAll(async ({ browser }) => {
+    const token = await getToken(
+      CREDENTIALS.LOCAL_REGISTRAR.USERNAME,
+      CREDENTIALS.LOCAL_REGISTRAR.PASSWORD
+    )
+    const res = await createDeclaration(token)
+    declaration = res.declaration
+    page = await browser.newPage()
+  })
+
+  test.afterAll(async () => {
+    await page.close()
+  })
+
+  test('Log in', async () => {
+    await loginToV2(page)
+  })
+
+  test('Go to review', async () => {
+    await page.getByRole('button', { name: 'Ready to print' }).click()
+    await navigateToCertificatePrintAction(page, declaration)
+    await selectCertificationType(page, 'Death Certificate')
+    await selectRequesterType(page, 'Print and issue to Informant (Spouse)')
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await page.getByRole('button', { name: 'Verified' }).click()
+    await page.getByRole('button', { name: 'Continue' }).click()
+  })
+
+  test('Validate deceased name', async () => {
+    await expectInPdf(
+      page,
+      `${declaration['deceased.name'].firstname} ${declaration['deceased.name'].surname}`
+    )
+  })
+
+  test('Validate deceased place of death', async () => {
+    await expectInPdf(page, 'Ibombo, Central, Farajaland')
+  })
+
+  test('Validate registrar name', async () => {
+    await expectInPdf(page, 'Kennedy Mweene')
+  })
+})
+
 test.describe
   .serial("Validate 'Death Certificate Certified Copy' PDF details", () => {
   let page: Page
@@ -48,48 +100,26 @@ test.describe
     await page.getByRole('button', { name: 'Continue' }).click()
   })
 
-  test('Validate deceased place of death', async () => {
-    await expect(page.locator('#print')).toContainText(
-      'Ibombo, Central, Farajaland'
+  test('Validate deceased name', async () => {
+    await expectInPdf(
+      page,
+      `${declaration['deceased.name'].firstname} ${declaration['deceased.name'].surname}`
     )
-  })
-})
-
-test.describe.serial("Validate 'Death Certificate' PDF details", () => {
-  let page: Page
-  let declaration: Declaration
-
-  test.beforeAll(async ({ browser }) => {
-    const token = await getToken(
-      CREDENTIALS.LOCAL_REGISTRAR.USERNAME,
-      CREDENTIALS.LOCAL_REGISTRAR.PASSWORD
-    )
-    const res = await createDeclaration(token)
-    declaration = res.declaration
-    page = await browser.newPage()
-  })
-
-  test.afterAll(async () => {
-    await page.close()
-  })
-
-  test('Log in', async () => {
-    await loginToV2(page)
-  })
-
-  test('Go to review', async () => {
-    await page.getByRole('button', { name: 'Ready to print' }).click()
-    await navigateToCertificatePrintAction(page, declaration)
-    await selectCertificationType(page, 'Death Certificate')
-    await selectRequesterType(page, 'Print and issue to Informant (Spouse)')
-    await page.getByRole('button', { name: 'Continue' }).click()
-    await page.getByRole('button', { name: 'Verified' }).click()
-    await page.getByRole('button', { name: 'Continue' }).click()
   })
 
   test('Validate deceased place of death', async () => {
-    await expect(page.locator('#print')).toContainText(
-      'Ibombo, Central, Farajaland'
+    await expectInPdf(page, 'Ibombo, Central, Farajaland')
+  })
+
+  test('Validate registrar name', async () => {
+    await expectInPdf(page, 'Registrar: Kennedy Mweene')
+  })
+
+  test('Validate spouse name', async () => {
+    await expectInPdf(
+      page,
+      `${declaration['deceased.name'].firstname} ${declaration['deceased.name'].surname}`
     )
+    await expectInPdf(page, 'Spouse')
   })
 })
