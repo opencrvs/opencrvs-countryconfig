f0d9277c980f4fb7aaddd47f3f460e96bc39b6ad
jamil314 <jamil31415926@gmail.com>
2025-10-15 17:04:11 +0600

    e2e: update advanced search place of death tests

--- e2e/testcases/v2-advanced-search/8-search-death-location.spec.ts
+++ e2e/testcases/v2-advanced-search/8-search-death-location.spec.ts
@@ -1,26 +1,23 @@
 import { expect, test, type Page } from '@playwright/test'
 import { loginToV2 } from '../../helpers'
 
-test.describe
-  .serial("Advanced Search 8 - Death - Deceased's place of death", () => {
-  let page: Page
+const assertPills = async (page: Page, pills: string[]) => {
+  pills.push('Event: Death')
+  for (const pill of pills) {
+    await expect(page.getByText(pill)).toBeVisible()
+  }
+}
 
-  test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage()
-  })
-
-  test.afterAll(async () => {
-    await page.close()
-  })
-
-  test('8.1 Navigate to Advanced Search - Death - Event details', async () => {
+test.describe("Advanced Search 8 - Death - Deceased's place of death", () => {
+  test.beforeEach(async ({ page }) => {
     await loginToV2(page)
     await page.click('#searchType')
     await expect(page).toHaveURL(/.*\/advanced-search/)
     await page.getByText('Death').click()
     await page.getByText('Event details').click()
   })
-  test('8.2 Before selecting a place of death', async () => {
+
+  test('8.1 Before selecting a place of death', async ({ page }) => {
     await expect(
       page.getByText('Place of death', { exact: true })
     ).toBeVisible()
@@ -31,7 +28,7 @@ test.describe
     ).not.toBeVisible()
     await expect(page.getByText('Country')).not.toBeVisible()
   })
-  test('8.3 Select Health Institution', async () => {
+  test('8.2 Select Health Institution', async ({ page }) => {
     await page.getByTestId('select__eventDetails____placeOfDeath').click()
     await page.getByText('Health Institution', { exact: true }).click()
     await expect(
@@ -39,21 +36,39 @@ test.describe
         .locator('#eventDetails____deathLocation-form-input')
         .getByText('Health Institution', { exact: true })
     ).toBeVisible()
+
     await expect(page.getByText('Country')).not.toBeVisible()
+
+    await page.locator('#eventDetails____deathLocation').fill('water')
+    await page.getByText('Water FallsRural Health Centre').click()
+
+    await page.getByTestId('search').click()
+    await expect(page.getByText(/Search results\s*\(\d+\)/)).toBeVisible()
+
+    await assertPills(page, [
+      "Deceased's Health Institution: Water FallsRural Health Centre, Ienge, Pualula, Farajaland",
+      'Place of death: Health Institution'
+    ])
   })
-  test('8.4 Select Usual place of residence', async () => {
+  test('8.3 Select Residential address', async ({ page }) => {
     await page.getByTestId('select__eventDetails____placeOfDeath').click()
-    await page
-      .getByText("Deceased's usual place of residence", { exact: true })
-      .click()
+    await page.getByText('Residential address', { exact: true }).click()
     await expect(
       page
         .locator('#eventDetails____deathLocation-form-input')
         .getByText('Health Institution', { exact: true })
     ).not.toBeVisible()
-    await expect(page.getByText('Country')).not.toBeVisible()
+    await expect(page.getByText('Country')).toBeVisible()
+
+    await page.getByTestId('search').click()
+    await expect(page.getByText(/Search results\s*\(\d+\)/)).toBeVisible()
+
+    await assertPills(page, [
+      'Usual place of residence: Farajaland, Central, Ibombo',
+      'Place of death: Residential address'
+    ])
   })
-  test('8.4 Select Other', async () => {
+  test('8.4 Select Other', async ({ page }) => {
     await page.getByTestId('select__eventDetails____placeOfDeath').click()
     await page.getByText('Other', { exact: true }).click()
     await expect(
@@ -62,5 +77,12 @@ test.describe
         .getByText('Health Institution', { exact: true })
     ).not.toBeVisible()
     await expect(page.getByText('Country')).toBeVisible()
+
+    await page.getByTestId('search').click()
+    await expect(page.getByText(/Search results\s*\(\d+\)/)).toBeVisible()
+    await assertPills(page, [
+      'Death location address: Farajaland, Central, Ibombo',
+      'Place of death: Other'
+    ])
   })
 })
