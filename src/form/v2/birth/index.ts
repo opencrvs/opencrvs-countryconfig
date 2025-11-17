60340a91014c33245ff72918d86dd9b3a3e94c9f
cibelius <cihan.m.bebek@gmail.com>
2025-11-17 12:27:38 +0200

    update config

--- src/form/v2/birth/index.ts
+++ src/form/v2/birth/index.ts
@@ -228,7 +228,7 @@ export const birthEvent = defineConfig({
           'This is shown as the action name anywhere the user can trigger the action from',
         id: 'event.birth.action.approve.label'
       },
-      confirmationText: {
+      supportingCopy: {
         defaultMessage:
           'This birth has been registered late. You are now approving it for further validation and registration.',
         description: 'This is the confirmation text for the approve action',
@@ -239,7 +239,7 @@ export const birthEvent = defineConfig({
         { id: 'approval-required-for-late-registration', operation: 'remove' }
       ],
       auditHistoryLabel: {
-        defaultMessage: 'Approved late registration',
+        defaultMessage: 'Approved',
         description:
           'The label to show in audit history for the approve action',
         id: 'event.birth.action.approve.audit-history-label'
