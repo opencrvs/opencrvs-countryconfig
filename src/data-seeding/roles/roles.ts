97eeb71906ea5a6a51cafce9edb328c6ce7e1d15
Nil20 <nileeeem36@gmail.com>
2025-12-05 17:16:29 +0600

    chore: remove custom action scope permission from registrar

--- src/data-seeding/roles/roles.ts
+++ src/data-seeding/roles/roles.ts
@@ -221,8 +221,7 @@ export const roles: Role[] = [
       'record.register[event=birth|death|tennis-club-membership]',
       'record.registered.print-certified-copies[event=birth|death|tennis-club-membership]',
       'record.registered.correct[event=birth|death|tennis-club-membership]',
-      'record.unassign-others[event=birth|death|tennis-club-membership]',
-      'record.custom-action[event=birth,customActionType=Approve]'
+      'record.unassign-others[event=birth|death|tennis-club-membership]'
     ]
   },
   {
@@ -336,7 +335,7 @@ export const roles: Role[] = [
     },
     scopes: [
       'record.read[event=birth|death|tennis-club-membership]',
-      'record.custom-action[event=birth,customActionType=Approve]',
+      'record.custom-action[event=birth,customActionType=APPROVE_DECLARATION]',
       'workqueue[id=late-registration-approval-required]',
       'search[event=birth,access=all]'
     ]
