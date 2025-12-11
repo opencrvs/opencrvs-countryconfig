e1a4e1b801fa2aba5ab120037b36a62e0a878579
Nil20 <nileeeem36@gmail.com>
2025-12-11 16:45:35 +0600

    feat: add notes field to escalate actions

--- src/form/v2/birth/index.ts
+++ src/form/v2/birth/index.ts
@@ -397,6 +397,7 @@ export const birthEvent = defineConfig({
         {
           id: 'escalate-to',
           type: FieldType.SELECT,
+          required: true,
           label: {
             defaultMessage: 'Escalate to',
             description: 'This is the label for escalate to field',
@@ -422,6 +423,16 @@ export const birthEvent = defineConfig({
               value: 'REGISTRAR_GENERAL'
             }
           ]
+        },
+        {
+          id: 'reason',
+          type: FieldType.TEXTAREA,
+          required: true,
+          label: {
+            defaultMessage: 'Reason',
+            description: 'This is the label for reason field',
+            id: 'form.field.label.reasonNotApplying'
+          }
         }
       ],
       flags: [
@@ -453,7 +464,18 @@ export const birthEvent = defineConfig({
           'This is shown when the provincial registrar feedback can be triggered from the action from',
         id: 'event.birth.action.provincial-registrar-feedback.label'
       },
-      form: [],
+      form: [
+        {
+          id: 'notes',
+          type: 'TEXTAREA',
+          required: true,
+          label: {
+            defaultMessage: 'Comments',
+            description: 'This is the label for the field for a custom action',
+            id: 'event.birth.custom.action.approve.field.notes.label'
+          }
+        }
+      ],
       flags: [
         {
           id: 'escalated-to-provincial-registrar',
@@ -483,7 +505,18 @@ export const birthEvent = defineConfig({
           'This is shown when the registrar general feedback can be triggered from the action from',
         id: 'event.birth.action.registrar-general-feedback.label'
       },
-      form: [],
+      form: [
+        {
+          id: 'notes',
+          type: 'TEXTAREA',
+          required: true,
+          label: {
+            defaultMessage: 'Comments',
+            description: 'This is the label for the field for a custom action',
+            id: 'event.birth.custom.action.approve.field.notes.label'
+          }
+        }
+      ],
       flags: [
         {
           id: 'escalated-to-registrar-general',
