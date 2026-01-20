066a890dede7334da16bdac709531b6793be53a8
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-01-20 13:45:35 +0200

    Docker swarm to k8s migration

--- infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
+++ infrastructure/provision-server/hetzner-cloud-empty-server/servers.tf
@@ -4,8 +4,8 @@ module "opencrvs_single_node" {
   count       = (var.env_type == "single-node") ? 1 : 0
   source      = "./single-node"
   name        = "${var.country_name}-${var.env_name}"
-  location    = "hil"
-  server_type = "cpx41"
+  location    = "hel1"
+  server_type = "cx43"
   user_data = templatefile("cloud-init.yaml", {
     ssh_key = trimspace(file(".ssh/id_rsa.pub"))
   })
@@ -17,8 +17,8 @@ module "opencrvs_multi_node" {
   source       = "./multi-node"
   country_name = var.country_name
   env_name     = var.env_name
-  location     = "hil"
-  server_type  = "cpx41"
+  location     = "hel1"
+  server_type  = "cx43"
   user_data = templatefile("cloud-init.yaml", {
     ssh_key = trimspace(file(".ssh/id_rsa.pub"))
   })
