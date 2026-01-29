dc534d0b7355d6e2fbafee511851a8ae1644f190
tareq89 <tareqaziz.aust.cse@gmail.com>
2026-01-29 19:28:23 +0600

    Age of *relation label changed to Age (at time of event)

--- src/form/v2/death/forms/pages/deceased.ts
+++ src/form/v2/death/forms/pages/deceased.ts
@@ -154,9 +154,9 @@ export const deceased = defineFormPage({
       type: FieldType.AGE,
       required: true,
       label: {
-        defaultMessage: `Age of deceased`,
+        defaultMessage: `Age (at time of event)`,
         description: 'This is the label for the field',
-        id: 'event.death.action.declare.form.section.deceased.field.age.label'
+        id: 'form.field.label.age'
       },
       configuration: {
         asOfDate: field('eventDetails.date'),
