9000e1ff044a218aa626bb4086f8892eb2da9e88
jamil314 <jamil31415926@gmail.com>
2026-02-23 18:28:20 +0600

    fix: reindexing timeout

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,8 @@ import {
   CHECK_INVALID_TOKEN,
   AUTH_URL,
   DEFAULT_TIMEOUT,
-  GATEWAY_URL
+  GATEWAY_URL,
+  THIRTY_MINUTES_IN_MILISECOND
 } from '@countryconfig/constants'
 import {
   contentHandler,
@@ -207,9 +208,16 @@ export async function createServer() {
     port: COUNTRY_CONFIG_PORT,
     routes: {
       cors: { origin: whitelist },
-      payload: { maxBytes: 52428800, timeout: DEFAULT_TIMEOUT }
+      timeout: {
+        server: DEFAULT_TIMEOUT
+      },
+      payload: {
+        maxBytes: 52428800
+      }
     }
   })
+
+  server.listener.requestTimeout = THIRTY_MINUTES_IN_MILISECOND
 
   await server.register(getPlugins())
 
@@ -573,6 +581,10 @@ export async function createServer() {
       payload: {
         output: 'stream',
         parse: false
+      },
+      timeout: {
+        server: THIRTY_MINUTES_IN_MILISECOND,
+        socket: THIRTY_MINUTES_IN_MILISECOND
       }
     },
     handler: async (req, h) => {
@@ -709,16 +721,16 @@ export async function createServer() {
         actions: event.actions.map((action, index) =>
           index === event.actions.length - 1
             ? {
-                ...action,
-                status: ActionStatus.Accepted,
-                ...(actionType === ActionType.REGISTER
-                  ? {
-                      registrationNumber: (
-                        response.source as { registrationNumber: string }
-                      ).registrationNumber
-                    }
-                  : {})
-              }
+              ...action,
+              status: ActionStatus.Accepted,
+              ...(actionType === ActionType.REGISTER
+                ? {
+                  registrationNumber: (
+                    response.source as { registrationNumber: string }
+                  ).registrationNumber
+                }
+                : {})
+            }
             : action
         ) as ActionDocument[]
       }
