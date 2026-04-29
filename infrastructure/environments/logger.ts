274f7a750aac09dddfdb1a08f51f9342851a2690
Vadym Mudryi <vadym.mudryi@gmail.com>
2026-04-29 15:15:53 +0300

    cleanup

--- infrastructure/environments/logger.ts
+++
@@ -1,9 +0,0 @@
-import kleur from 'kleur'
-
-/* eslint-disable no-console */
-export const log = console.log
-export const warn = console.warn
-export const success = (...args: any[]) =>
-  console.log(kleur.green(args.join(' ')))
-export const info = console.info
-export const error = (...args: any[]) => console.log(kleur.red(args.join(' ')))
--- infrastructure/monitoring/kibana/README.md
+++
@@ -1,7 +0,0 @@
-# Kibana saved objects export
-
-When exporting a new config.ndjson from Kibana UI, make sure you only select the following items to export:
-
-- alert
-- config
-- infrastructure-ui-source
